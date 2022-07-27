077963e2d9208ef255f305f4eb2a8cfa99ef40ba
Cory <bowdencory8@gmail.com>
2022-07-27 10:24:13 -0400

    Working on setup

--- NodeApi/app.js
+++ NodeApi/app.js
@@ -1,2 +1,27 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
+
+//db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB Connection Error: ${err.message}`)
+})
+
+// Bring in routes
+const postRoutes = require("./routes/post")
+
+// middleware
+app.use(morgan('dev'))
+
+app.use("/", postRoutes);
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`A Node JS API is listening on port: ${port}`)})
